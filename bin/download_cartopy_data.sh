c955ef0eb4578febf4068b48b51fc8ca160ff6c0
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-05-05 13:31:22 +0200

    Use python script to download Antarctic ice shelves
    The `download_cartopy_data.sh` script now generates a small
    python script in the temp directory where `feature_download.py`
    is downloaded.  This script first adds `antarctic_ice_shelves_polys`
    to the list of features to download before performing the download
    with a direct call to the `download_features` function.

--- bin/download_cartopy_data.sh
+++ bin/download_cartopy_data.sh
@@ -20,10 +20,44 @@ mkdir ${tmp_dir}
 # cd to tmp dir, get cartopy source, download data to repo data path
 (
     cd ${tmp_dir}
-    wget https://raw.githubusercontent.com/SciTools/cartopy/v0.17.0/tools/feature_download.py
-    python feature_download.py \
-        --output ${target_dir} cultural-extra cultural gshhs physical \
-        --ignore-repo-data
+    version=`python -c "import cartopy; print(cartopy.__version__)"`
+    wget https://raw.githubusercontent.com/SciTools/cartopy/v${version}/tools/feature_download.py
+    touch __init__.py
+
+    cat <<EOF >> download_cartopy.py
+#!/usr/bin/env python
+import argparse
+import os
+import cartopy
+from cartopy import config
+from feature_download import FEATURE_DEFN_GROUPS, download_features
+
+
+def main(target_dir):
+
+    # add Antarctic ice shelves
+    FEATURE_DEFN_GROUPS['physical'] = \
+        FEATURE_DEFN_GROUPS['physical'] + \
+        (('physical', 'antarctic_ice_shelves_polys', ('50m', '10m')),)
+
+    config['pre_existing_data_dir'] = target_dir
+    config['data_dir'] = target_dir
+    config['repo_data_dir'] = target_dir
+    download_features(
+        ['cultural-extra', 'cultural', 'gshhs', 'physical'], dry_run=False)
+    os.chdir(owd)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Download cartopy data for caching.')
+    parser.add_argument('--output', '-o', required=True,
+                        help='save datasets in the specified directory')
+    args = parser.parse_args()
+    main(args.output)
+EOF
+
+    python download_cartopy.py -o ${target_dir}
 )
 
 # clean up
